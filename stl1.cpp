00fbce46ae2badefa1f083b1c99d630635e76244
John Doe <imh10037@bitmesra.ac.in>
2023-07-09 01:54:45 +0530

    i changed everything

--- stl1.cpp
+++ stl1.cpp
@@ -1,6 +1,25 @@
+//we will see how to initialize a vector and how to take input and output from it
 #include<iostream>
+#include<bits/stcd++.h> 
+
 using namespace std ; 
 int main(){
-    cout<<"Hello world";
+    int n; 
+    cin>>n;  // size of my vector that user defines 
+
+    vector<int> v; 
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+
+        v.push_back(x)
+    }
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" "; 
+
+    }
+
+    return 0; 
     
+
 }
