2e360d2db5b0146ad54b24f6acd7116e26eace25
Kanishk Gupta <imh10037@bitmesra.ac.in>
2023-07-11 00:50:16 +0530

    changed

---
+++ stl4.cpp
@@ -0,0 +1,28 @@
+// vector of pair 
+#include<iostream> 
+#include<bits/stdc++.h> 
+using namespace std; 
+void printvec(vector<pair<int,int> > v){
+    for(int i=0;i<v.size();i++){
+        cout<<v[i].first<<" "v[i].second;
+
+    }
+    cout<<endl; 
+
+}
+int main(){
+    int n; 
+    cin>>n; 
+    vector<pair<int,int> > v; 
+    
+    for(int i=0;i<n;i++){
+        int x,y;
+        cin>>x>>y;
+        v.push_back({x,y});
+
+    }
+
+    printvec(v);
+
+
+}
