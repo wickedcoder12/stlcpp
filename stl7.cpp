e1d8f9d6727cda557bb8c5b4bb54c9af64cf67a6
Kanishk Gupta <imh10037@bitmesra.ac.in>
2023-07-12 00:35:45 +0530

    change

---
+++ stl7.cpp
@@ -0,0 +1,24 @@
+// adding empty row in vector of vector 
+
+#include<iostream> 
+#include<bits/stdc++.h> 
+using namespace std; 
+int main(){
+    int N; 
+    cin>>N; 
+    vector<vector<int> > v;
+    for(int i=0;i<N;i++){
+        int n; 
+        cin>>n; 
+        vector<int> temp; 
+        for(int j=0;j<n;j++){
+            int x; 
+            cin>>x; 
+            temp.push_back(x);
+        }
+        v.push_back(temp);
+
+    }
+    v.push_back(vector<int> ());
+
+}
